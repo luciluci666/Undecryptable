a767ccacb1c24ab9be1adfc812645d9337a4e3e5
maryshhh <06dmv06@gmail.com>
2023-04-12 21:27:27 +0300

    FRONTEND | started registration form developing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({
+    name: "",
+    last_name: "",
+    username: "",
+    password: ""
+  })
+
+  const submit = (event) => {
+    event.preventDefault()
+    axios.post('http://127.0.0.1:8000/register',user).then((res) => {
+      console.log(res.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <input 
+          value={user.name}
+          onChange={(event) => setUser({...user, name: event.target.value})}
+          type='text'
+          placeholder='First name'/>
+        <input 
+          value={user.last_name}
+          onChange={(event) => setUser({...user, last_name: event.target.value})}
+          type='text' 
+          placeholder='Last name'/>
+        <input 
+          value={user.username}
+          onChange={(event) => setUser({...user, username: event.target.value})}
+          type='email' 
+          placeholder='Email'/>
+        <input 
+          value={user.password} 
+          onChange={(event) => {setUser({...user, password: event.target.value})}} 
+          type='password' 
+          placeholder='Password'/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
